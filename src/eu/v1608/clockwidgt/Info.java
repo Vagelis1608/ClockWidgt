f97301fb8c8125293b33f23907b01e56b2ae948d
root <root@localhost>
2014-02-08 17:34:13 +0200

    Initial commit

---
+++ src/eu/v1608/clockwidgt/Info.java
@@ -0,0 +1,12 @@
+package eu.v1608.clockwidgt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+	}
+}
